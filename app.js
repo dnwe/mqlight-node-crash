c9dd786e2cbdac2f0f1c682d097cd18728989783
Walter Higgins <WALTERH4@ie.ibm.com>
2016-04-18 11:59:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
 var express = require('express'),
     http = require('http'),
-    multer = require('multer'),
     mqlight = require('mqlight'),
-    KB = 1024,
-    MB = 1024 * KB;
-
-var multerUpload = multer({
-  storage: multer.memoryStorage(),
-  limit: {
-    fileSize: 10 * MB,
-    files: 1
-  }
-}).single('uploadMe');
+    busboy = require('connect-busboy'),
+    mqClient = mqlight.createClient( { service: 'amqp:localhost' } ),
+    app = express(),
+    server = http.Server(app);
 
 function sendMsg(){
   var msg = 'Time:' + new Date();
   mqClient.send('theQueue', msg);
 }
-
-var mqClient = mqlight.createClient( { service: 'amqp:localhost' } );
-var app = express();
-
-function onUploadRequest(req, res, next){
-
-  function onUploadComplete(uploadError){
-    if (uploadError){
-      res.json({ status: 'error', error: uploadError});
-      return;
-    }
-    res.json({status: 'ok' });
-  }
-
+app.use(busboy());
+function onUploadRequestBusboy(req, res, next){
   res.on('finish', sendMsg);
-
-  multerUpload(req, res, onUploadComplete);
+  req.pipe(req.busboy);
+  req.busboy.on('finish', function(){
+    res.json({ status: 'ok'});
+  });
 }
-app.use(express.static('public'));
-app.post('/upload', onUploadRequest);
-app.get('/other', function(req, res, next){
-  res.on('finish', sendMsg);
-  res.json({status: 'other is ok'});
-});
-
-var server = require('http').Server(app);
-
+app.post('/upload', onUploadRequestBusboy);
 server.listen(5000, 'localhost');
