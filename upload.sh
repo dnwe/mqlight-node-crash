5af401613b2da51fecec71b7f1b7a1a16d50cf63
Dominic Evans <dominic.evans@uk.ibm.com>
2016-04-19 11:47:36 +0100

    Exit from upload.sh if connection refused
    Also add a max-time to prevent hangs

--- upload.sh
+++ upload.sh
@@ -3,5 +3,8 @@
 while true; do
   for NUM in {0..8}; do
     echo 'http://127.0.0.1:5000/upload'
-  done | xargs -n1 -P8 -I {} curl -s -F "uploadMe=@./fixtures/test.txt" {} >/dev/null
+  done | xargs -n1 -P8 -I {} \
+    curl -s --max-time 3 -F "uploadMe=@./fixtures/test.txt" {} >/dev/null
+  RC=$?
+  [ $RC -eq 0 ] || nc -z 127.0.0.1 5000 || exit $RC
 done;
